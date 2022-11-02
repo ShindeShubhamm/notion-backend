66e764f38110716eb34928c6901909b9f4396abe
shindeshubhamm <shubhamshinde.3151@gmail.com>
2022-11-02 13:42:27 +0100

    Netlify setup

---
+++ src/server.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const cors = require('cors');
+const express = require('express');
+const serverless = require('serverless-http');
+const { dataToBase64 } = require('./utils/helpers');
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+
+// CORS
+app.use(cors());
+const whitelist = [
+    'http://localhost:3000',
+    'https://notion-spreadsheets.netlify.app',
+];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+const clientId = process.env.NOTION_OAUTH_CLIENTID;
+const clientSecret = process.env.NOTION_OAUTH_CLIENTSECRET;
+
+router.get('/', (req, res) => {
+    res.send({ message: 'Hello world!' });
+});
+
+router.post('/accesstoken', cors(corsOptions), async (req, res) => {
+    const { code } = req.body;
+    const auth = dataToBase64(`${clientId}:${clientSecret}`);
+    try {
+        const response = await axios.post(
+            'https://api.notion.com/v1/oauth/token',
+            {
+                code,
+                grant_type: 'authorization_code',
+                redirect_uri: 'http://localhost:3000',
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Basic ${auth}`,
+                },
+            }
+        );
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.use('/', router);
+app.use('/.netlify/functions/server', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
